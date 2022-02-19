0f018fd4342e3d38a1e23a330a9809ea5e3c5c2f
Huzaif <53694549+HuzaifDharejo@users.noreply.github.com>
2022-02-19 21:15:54 +0500

    testcases of wondowes cmds

--- cypress/integration/checkWindowscmds.spec.js
+++ cypress/integration/checkWindowscmds.spec.js
@@ -3,11 +3,19 @@ describe("Checking chrome windows cmandes", () => {
     cy.visit("#/login?_k=5jewux");
   });
   it("checking hash in urls", () => {
-      cy.hash().should("eq", "#/login?_k=5jewux")
-      cy.location("host").should("eq", "react-redux.realworld.io")
-      cy.location("protocol").should("eq", "https:")
-      cy.location("pathname").should("eq", "/")
+    cy.hash().should("eq", "#/login?_k=5jewux");
+    cy.location("host").should("eq", "react-redux.realworld.io");
+    cy.location("protocol").should("eq", "https:");
+    cy.location("pathname").should("eq", "/");
 
-      cy.reload()
+    cy.reload();
+  });
+
+  it.only("navigating inside windows with cypress cmdr", () => {
+    cy.contains("Sign up").click();
+    cy.go("back");
+    cy.contains("Need an account?").should("have.text", "Need an account?");
+    cy.go("forward");
+    cy.contains("Have an account?").should("have.text", "Have an account?");
   });
 });
